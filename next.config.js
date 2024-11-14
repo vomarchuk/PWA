17bbd3e3641e2bb1e89f1ff0c800dab368a4a2ab
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2024-11-14 09:49:53 +0100

    push with master

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
 };
 const withPWA = require('next-pwa')({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
 });
 
 module.exports = withPWA(nextConfig);
